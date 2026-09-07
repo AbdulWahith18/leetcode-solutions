ff9a933e3187db4ad09970a3017b0677e535a901
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-07 07:09:48 +0530

    ✅ Smallest Stable Index II | Medium | Java | Runtime: 0 ms | Memory: 42.1 MB | 2026-09-05

--- medium/3904-smallest-stable-index-ii/solution.java
+++ medium/3904-smallest-stable-index-ii/solution.java
@@ -1,24 +1,29 @@
-        int m = nums[0];
-        for(int i = 0; i < l; i++) 
-        {
-            m = Math.max(m, nums[i]);
-            max[i] = m;
-        }
-        int n = nums[l - 1];
-        for(int i = l - 1; i >= 0; i--)
-        {
-            n = Math.min(n, nums[i]);
-            min[i] = n;
-        }
-        int res=-1,com=Integer.MAX_VALUE;
-        for(int i = 0; i < l; i++) 
-        {
-            if(max[i] - min[i] <= k&&max[i]-min[i]<com)
-            {
-                com=max[i]-min[i];
-                res=i;
-            }       
-        }
-        return res;
-    }
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int l = nums.length;
+        int[] max = new int[l];
+        int[] min = new int[l];
+        int m = nums[0];
+        for(int i = 0; i < l; i++) 
+        {
+            m = Math.max(m, nums[i]);
+            max[i] = m;
+        }
+        int n = nums[l - 1];
+        for(int i = l - 1; i >= 0; i--)
+        {
+            n = Math.min(n, nums[i]);
+            min[i] = n;
+        }
+        int res=-1,com=Integer.MAX_VALUE;
+        for(int i = 0; i < l; i++) 
+        {
+            if(max[i] - min[i] <= k&&max[i]-min[i]<com)
+            {
+                com=max[i]-min[i];
+                res=i;
+            }       
+        }
+        return res;
+    }
 }
