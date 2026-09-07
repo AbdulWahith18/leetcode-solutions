fbf2ea12435e5fe60a908b177049d88ca9b5a5ce
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-07 07:11:36 +0530

    ✅ Smallest Stable Index II | Medium | Java | Runtime: 5 ms (66.41%) | Memory: 126.7 MB | 2026-09-05

--- medium/3904-smallest-stable-index-ii/solution.java
+++ medium/3904-smallest-stable-index-ii/solution.java
@@ -18,9 +18,9 @@ class Solution {
         int res=-1,com=Integer.MAX_VALUE;
         for(int i = 0; i < l; i++) 
         {
-            if(max[i] - min[i] <= k&&max[i]-min[i]<com)
+            if(max[i] - min[i] <= k&&i<com)
             {
-                com=max[i]-min[i];
+                com=i;
                 res=i;
             }       
         }
